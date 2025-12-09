a33d0c5d9bc2dfb5155c0e50ff25adeb8cea931a
Carl van Mastrigt <carl.van.mastrigt@gmail.com>
2025-12-09 21:58:30 +1100

    allow non power of 2 sizes in buddy tree

--- data_structures/buddy_tree.h
+++ data_structures/buddy_tree.h
@@ -26,14 +26,20 @@ struct sol_buddy_tree
 	/** it is the callers responsibility to size/scale provided offsets 
 	 * which will always be in single units from the buddy tree's perspective */
 	uint32_t* availablity_masks;
-	// uint32_t size_exponent;
-	uint32_t size_bit;
+	
+	/** the power of 2 that includes all addressible offsets in the tree, is also double the size of the */
+	uint32_t encompasing_bit;
+
+	/** this marks the size of the allocation managed in cases where the allocation didnt start out as a power of 2 or long lived allocations were taken from the end */
+	uint32_t size;
 };
 
-void sol_buddy_tree_initialise(struct sol_buddy_tree* tree, uint32_t size_exponent);
+void sol_buddy_tree_initialise(struct sol_buddy_tree* tree, uint32_t size);
 void sol_buddy_tree_terminate(struct sol_buddy_tree* tree);
 
-
-bool sol_buddy_tree_acquire(struct sol_buddy_tree* tree, uint32_t desired_exponent, uint32_t* allocation_offset);
-
+bool sol_buddy_tree_acquire(struct sol_buddy_tree* tree, uint32_t desired_size_exponent, uint32_t* allocation_offset);
 void sol_buddy_tree_release(struct sol_buddy_tree* tree, uint32_t allocation_offset);
+
+/** allowing dynamic resizing of total space, while possible is a bad idea, 
+ * as the slots opened up by any such resizing will be preferentially used/split
+ * thus any subsequent resizing is very unlikely to work as the space it would try to take is the most likely region to have been allocated */ 
